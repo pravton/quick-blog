93ccdaae039f4c1bd7fa3e0ebc1ad6af9344d67a
Clinton <80900245+pravton@users.noreply.github.com>
2022-03-24 13:21:11 -0400

    initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function HomePage(props) {
+  return (
+    <>
+    <h1>Quick Blog!</h1> 
+    <section>
+      <h2>Title</h2>
+      <p>lorem ipsum dolor sit amet, consectetur adip</p>
+      <p>lorem ipsum dolor sit amet, consectetur adip</p>
+      <p>lorem ipsum dolor sit amet, consectetur adip</p>
+      <p>lorem ipsum dolor sit amet, consectetur adip</p>
+    </section>
+    </>
+  );
+}
+
+export default HomePage;
